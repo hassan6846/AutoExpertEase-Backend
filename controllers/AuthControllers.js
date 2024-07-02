defc9db8500b37259f2c7c8484e9145683ec306a
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-07-02 19:24:46 +0500

    main

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -53,10 +53,7 @@ const VerifyEmail = async (req, res, next) => {
             // OTP is valid
      
             return res.status(200).json({ message: "OTP verified successfully" });
-        } else {
-            // OTP is invalid
-            return res.status(400).json({ error: "Invalid OTP" });
-        }
+        } 
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" });
